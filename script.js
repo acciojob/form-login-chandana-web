5768dd4caf945512ad4fd6fea6e928ef8a5db93d
chandana-web <143087553+chandana-web@users.noreply.github.com>
2025-01-27 07:09:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
 
+  const first = document.getElementsByName("fname")[0].value;
+  const last = document.getElementsByName("lname")[0].value;
+
+  alert(`${first}`+" " +`${last}`);
+  return false;
 }
